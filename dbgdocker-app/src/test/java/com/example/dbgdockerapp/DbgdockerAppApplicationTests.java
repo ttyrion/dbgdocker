a79e5fad2d767f913358deabc9afb1fbb952d8bb
ttyrion <tyrion.marting@hotmail.com>
2022-06-07 16:39:15 +0800

    maven ${revision} : https://maven.apache.org/maven-ci-friendly.html

---
+++ dbgdocker-app/src/test/java/com/example/dbgdockerapp/DbgdockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbgdockerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbgdockerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ dbgdocker-controller/src/main/java/com/example/dbgdockercontroller/DbgdockerControllerApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbgdockercontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbgdockerControllerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbgdockerControllerApplication.class, args);
+    }
+
+}
